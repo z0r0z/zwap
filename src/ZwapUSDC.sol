74d7b17a5372812a3ddcc440d84062eb015e2f99
ross <92001561+z0r0z@users.noreply.github.com>
2024-07-09 15:08:33 +0000

    ⚡

--- src/ZwapUSDC.sol
+++ src/ZwapUSDC.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity 0.8.26;
 
-/// @notice Swap any ETH sent to this contract into USDC at market rate.
 contract ZwapUSDC {
-    bytes32 constant WETH = 0x000000000000000000000000C02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
-    bytes32 constant POOL = 0x00000000000000000000000088e6A0c2dDD26FEEb64F039a2c41296FcB3f5640;
-    uint256 constant MAX_SQRT_RATIO_MINUS_ONE = 1461446703485210103287273052203988822378723970341;
+    address constant POOL = 0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640;
+    address constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+
+    constructor() payable {}
 
     receive() external payable {
         assembly ("memory-safe") {
@@ -13,10 +13,9 @@ contract ZwapUSDC {
             mstore(0x04, caller())
             mstore(0x24, 0)
             mstore(0x44, callvalue())
-            mstore(0x64, MAX_SQRT_RATIO_MINUS_ONE)
+            mstore(0x64, 1461446703485210103287273052203988822378723970341)
             mstore(0x84, 0xa0)
-            mstore(0xa4, 0)
-            pop(call(gas(), POOL, 0, 0x00, 0xc4, codesize(), 0x00))
+            pop(call(gas(), POOL, 0, 0x00, 0x104, codesize(), 0x00))
         }
     }
 
@@ -29,7 +28,6 @@ contract ZwapUSDC {
             mstore(0x34, amount1Delta)
             mstore(0x00, 0xa9059cbb000000000000000000000000)
             pop(call(gas(), WETH, 0, 0x10, 0x44, codesize(), 0x00))
-            mstore(0x34, 0)
         }
     }
 }
