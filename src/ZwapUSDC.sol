d257a3866bfb084734446679a158c9b287b5746c
ross <92001561+z0r0z@users.noreply.github.com>
2024-07-09 11:45:55 +0000

    ⚡

--- src/ZwapUSDC.sol
+++ src/ZwapUSDC.sol
@@ -4,7 +4,6 @@ pragma solidity 0.8.26;
 /// @notice Swap any ETH sent to this contract into USDC at market rate.
 contract ZwapUSDC {
     address constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
-    address constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
     address constant POOL = 0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640;
     uint160 constant MAX_SQRT_RATIO_MINUS_ONE = 1461446703485210103287273052203988822378723970341;
 
