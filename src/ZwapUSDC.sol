556e7cc47d62bc042542b0a214788c6ff0f9bc45
ross <92001561+z0r0z@users.noreply.github.com>
2024-07-10 11:35:06 +0000

    💸 drops

--- src/ZwapUSDC.sol
+++ src/ZwapUSDC.sol
@@ -1,16 +1,25 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity 0.8.26;
 
-/// @notice Swap ETH to USDC for any senders.
+/// @notice Swap ETH to USDC and make payments.
 contract ZwapUSDC {
     address constant POOL = 0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640;
+    address constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
     address constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
     uint160 constant MAX_SQRT_RATIO_MINUS_ONE = 1461446703485210103287273052203988822378723970341;
 
     constructor() payable {}
 
     receive() external payable {
-        ISwap(POOL).swap(msg.sender, false, int256(msg.value), MAX_SQRT_RATIO_MINUS_ONE, "");
+        zwap(msg.sender, int256(msg.value));
+    }
+
+    function zwap(address to, int256 amount) public payable {
+        assembly ("memory-safe") {
+            if iszero(amount) { amount := callvalue() }
+        }
+        ISwap(POOL).swap(to, false, amount, MAX_SQRT_RATIO_MINUS_ONE, "");
+        _repay(address(this).balance);
     }
 
     fallback() external payable {
@@ -23,9 +32,37 @@ contract ZwapUSDC {
             pop(call(gas(), WETH, 0, 0x10, 0x44, codesize(), 0x00))
         }
     }
+
+    function _repay(uint256 dust) internal {
+        assembly ("memory-safe") {
+            if dust { pop(call(gas(), caller(), dust, codesize(), 0x00, codesize(), 0x00)) }
+        }
+    }
+
+    struct Drop {
+        address to;
+        uint96 amount;
+    }
+
+    function zwapDrop(Drop[] calldata drops, uint256 sum) public payable {
+        zwap(address(this), -int256(sum));
+        for (uint256 i; i != drops.length; ++i) {
+            assembly ("memory-safe") {
+                mstore(0x00, 0xa9059cbb000000000000000000000000)
+            }
+            _transfer(drops[i].to, drops[i].amount);
+        }
+    }
+
+    function _transfer(address to, uint96 amount) internal {
+        assembly ("memory-safe") {
+            mstore(0x14, to)
+            mstore(0x34, amount)
+            pop(call(gas(), USDC, 0, 0x10, 0x44, codesize(), 0x00))
+        }
+    }
 }
 
-/// @dev Simple Uniswap V3 swapping interface.
 interface ISwap {
     function swap(address, bool, int256, uint160, bytes calldata)
         external
