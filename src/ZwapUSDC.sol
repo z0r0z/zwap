75f716352b58f05e836fa30872915a99cfa61435
ross <92001561+z0r0z@users.noreply.github.com>
2024-07-13 07:27:12 +0000

    🍪 USDC crumb return

--- src/ZwapUSDC.sol
+++ src/ZwapUSDC.sol
@@ -49,6 +49,7 @@ contract ZwapUSDC {
         for (uint256 i; i != drops.length; ++i) {
             _transfer(drops[i].to, drops[i].amount);
         }
+        _transfer(msg.sender, _balanceOfThis());
     }
 
     function _transfer(address to, uint256 amount) internal {
@@ -57,6 +58,15 @@ contract ZwapUSDC {
             mstore(0x14, to)
             mstore(0x34, amount)
             pop(call(gas(), USDC, 0, 0x10, 0x44, codesize(), 0x00))
+        }
+    }
+
+    function _balanceOfThis() internal view returns (uint256 amount) {
+        assembly ("memory-safe") {
+            mstore(0x00, 0x70a08231000000000000000000000000)
+            mstore(0x14, address())
+            pop(staticcall(gas(), USDC, 0x10, 0x24, 0x20, 0x20))
+            amount := mload(0x20)
         }
     }
 }
